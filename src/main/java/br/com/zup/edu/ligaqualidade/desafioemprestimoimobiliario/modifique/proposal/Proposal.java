c2cec70ae3a87d0eecbc4a5980d1f56ac3f2fe32
Elias Lima <eliaslima900@hotmail.com>
2021-03-06 12:51:57 -0300

    desafio emprestimo imobiliario

--- src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/proposal/Proposal.java
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioemprestimoimobiliario/modifique/proposal/Proposal.java
@@ -1,12 +1,18 @@
 package br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.proposal;
 
 import java.math.BigDecimal;
+import java.util.List;
+
+import br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.proponent.Proponent;
+import br.com.zup.edu.ligaqualidade.desafioemprestimoimobiliario.modifique.warranty.Warranty;
 
 public class Proposal {
 
 	private String proposal_id;
 	private BigDecimal proposal_loan_value;
 	private Integer proposal_number_of_monthly_installments;
+	private List<Warranty> warranties;
+	private List<Proponent> proponents;
 
 	public Proposal(String proposal_id, BigDecimal proposal_loan_value,
 			Integer proposal_number_of_monthly_installments) {
@@ -25,6 +31,14 @@ public class Proposal {
 
 	public Integer getProposal_number_of_monthly_installments() {
 		return proposal_number_of_monthly_installments;
+	}
+
+	public List<Warranty> getWarranties() {
+		return warranties;
+	}
+
+	public List<Proponent> getProponents() {
+		return proponents;
 	}
 
 }
